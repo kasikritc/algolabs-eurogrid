5cfa184363c7e4c0f20a8b22f7ad946f5d66366e
kasikritc <kasikrit.c@outlook.com>
2024-03-06 23:59:41 -0500

    First working version

---
+++ helper_functions.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import streamlit as st
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import datetime
+
+def load_trade_data(file_path):
+    trade_data = pd.read_csv(file_path)
+    trade_data['Open DateTime'] = pd.to_datetime(trade_data['Open Time'], format='%Y-%m-%d %H:%M:%S')
+    trade_data['Close DateTime'] = pd.to_datetime(trade_data['Close Time'], format='%Y-%m-%d %H:%M:%S')
+    return trade_data
+
+def load_tick_data(file_path):
+    tf_ohlc_data = pd.read_csv(file_path)
+    tf_ohlc_data['DateTime'] = pd.to_datetime(tf_ohlc_data['DateTime'])
+    tf_ohlc_data.set_index('DateTime', inplace=True)
+    return tf_ohlc_data
+
+def align_datetime_to_candle(dt, timeframe):
+    """Aligns datetime to the start of the candlestick based on the timeframe."""
+    if timeframe == 1:
+        dt = dt.replace(second=0, microsecond=0)
+    elif timeframe == 5:
+        intervals = dt.minute // 5
+        dt = dt.replace(minute=intervals * 5, second=0, microsecond=0)
+    elif timeframe == 15:
+        intervals = dt.minute // 15
+        dt = dt.replace(minute=intervals * 15, second=0, microsecond=0)
+    elif timeframe == 60:
+        dt = dt.replace(minute=0, second=0, microsecond=0)
+    elif timeframe == 240:
+        intervals = dt.hour // 4
+        dt = dt.replace(hour=intervals * 4, minute=0, second=0, microsecond=0)
+    elif timeframe == 1440:
+        dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    return dt
+
+def adjust_start_end_datetimes(start_dt, end_dt, selected_timeframe, tf_ohlc_data):
+    """Adjusts the start and end datetimes for the chart based on user input."""
+    start_dt = align_datetime_to_candle(start_dt, selected_timeframe)
+    end_dt = align_datetime_to_candle(end_dt, selected_timeframe)
+    return (max(start_dt, tf_ohlc_data.index[0]), min(end_dt, tf_ohlc_data.index[-1]))
+
+def create_candlestick_figure(filtered_data):
+    """Creates the candlestick figure with the filtered data."""
+    fig = go.Figure(data=[go.Candlestick(
+        x=filtered_data.index,
+        open=filtered_data['Open'],
+        high=filtered_data['High'],
+        low=filtered_data['Low'],
+        close=filtered_data['Close'],
+        name='Price'
+    )])
+    return fig
+
+def add_price_markers(fig, start_dt, end_dt, open_price, close_price, trade_type):
+    """Adds markers for open and Closing Prices to the figure."""
+
+    fig.add_trace(go.Scatter(
+        x=[start_dt],
+        y=[open_price],
+        mode='markers',
+        marker_symbol='triangle-up' if trade_type == 'buy' else 'triangle-down',
+        marker_color='blue' if trade_type == 'buy' else 'red',
+        marker_size=10,
+        name='Opening Price Marker'
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=[end_dt],
+        y=[close_price],
+        mode='markers',
+        marker_symbol='triangle-down' if trade_type == 'buy' else 'triangle-up',
+        marker_color='white',
+        marker_size=10,
+        name='Closing Price Marker'
+    ))
+
+    return fig
+
+def add_tp_sl_lines(fig, start_dt, end_dt, tp_price, sl_price):
+    """Adds Take Profit and Stop Loss lines to the figure."""
+    # Add the TP and SL lines
+    fig.add_trace(go.Scatter(
+        x=[start_dt, end_dt],
+        y=[tp_price, tp_price],
+        mode='lines',
+        line=dict(color='green', width=2, dash='dot'),
+        name='Take Profit Line'
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=[start_dt, end_dt],
+        y=[sl_price, sl_price],
+        mode='lines',
+        line=dict(color='red', width=2, dash='dot'),
+        name='Stop Loss Line'
+    ))
+
+    mid_dt = start_dt + (end_dt - start_dt) / 2
+
+    # Add annotations for TP and SL lines
+    fig.add_annotation(
+        x=mid_dt,
+        y=tp_price,
+        text="TP",
+        showarrow=False,
+        bordercolor="green"
+    )
+    fig.add_annotation(
+        x=mid_dt,
+        y=sl_price,
+        text="SL",
+        showarrow=False,
+        bordercolor="red"
+    )
+
+    return fig
+
+def create_candlestick_chart(tf_ohlc_data, selected_trades, selected_timeframe, input_settings):
+    # Determine the number of minutes for the selected timeframe
+    timeframe_minutes = {
+        '1M': 1,
+        '5M': 5,
+        '15M': 15,
+        '1H': 60,
+        '4H': 240,
+        '1D': 1440
+    }
+    first_trade_start_datetime, last_trade_end_datetime = adjust_start_end_datetimes(selected_trades["Open DateTime"].iloc[0], selected_trades["Close DateTime"].iloc[-1], timeframe_minutes[selected_timeframe], tf_ohlc_data)
+
+    # Filter the data for the chart
+    chart_start_datetime = first_trade_start_datetime - pd.Timedelta(minutes=timeframe_minutes[selected_timeframe] * input_settings["Candles Before"])
+    chart_end_datetime = last_trade_end_datetime + pd.Timedelta(minutes=timeframe_minutes[selected_timeframe] * input_settings["Candles After"])
+    filtered_data = tf_ohlc_data.loc[chart_start_datetime:chart_end_datetime]
+    # Create the candlestick figure and add markers and lines
+    fig = create_candlestick_figure(filtered_data)
+
+    for index, trade in selected_trades.iterrows():
+        trade_start_datetime, trade_end_datetime = adjust_start_end_datetimes(trade["Open DateTime"], trade["Close DateTime"], timeframe_minutes[selected_timeframe], tf_ohlc_data)
+        fig = add_price_markers(fig, trade_start_datetime, trade_end_datetime, trade["Opening Price"], trade["Closing Price"], trade["Type"])
+        # fig = add_tp_sl_lines(fig, trade_start_datetime, trade_end_datetime, trade["T / P"], trade["S / L"])
+    
+    fig.update_layout(
+        title={
+            'text': 'Trade Progress',
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
+        xaxis_title='Time',
+        yaxis_title='Price',
+        xaxis_rangeslider_visible=False,
+        showlegend=False
+    )
+    return fig
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ from helper_functions import *
 if __name__ == "__main__":
     st.title('Trade Data and Candlestick Chart Viewer')
 
+    trade_number = int(st.number_input('Choose trade number', value=0, min_value=0, max_value=1758, step=1))
+
     # Add radio button for timeframe selection
     selected_timeframe = st.radio(
         "Select Timeframe",
@@ -14,56 +16,36 @@ if __name__ == "__main__":
 
     # Map the selection to the corresponding file
     file_mapping = {
-        '1M': '1M_XAUUSD_OHLC_dropna.csv',
-        '5M': '5M_XAUUSD_OHLC_dropna.csv',
-        '15M': '15M_XAUUSD_OHLC_dropna.csv',
-        '1H': '1H_XAUUSD_OHLC_dropna.csv',
-        '4H': '4H_XAUUSD_OHLC_dropna.csv',
-        '1D': '1D_XAUUSD_OHLC_dropna.csv'
+        '1M': '1M_EURUSD_OHLC_dropna.csv',
+        '5M': '5M_EURUSD_OHLC_dropna.csv',
+        '15M': '15M_EURUSD_OHLC_dropna.csv',
+        '1H': '1H_EURUSD_OHLC_dropna.csv',
+        '4H': '4H_EURUSD_OHLC_dropna.csv',
+        '1D': '1D_EURUSD_OHLC_dropna.csv'
     }
 
-    trade_data_file_path = 'clean_trade_data.csv'
+    trade_data_file_path = 'trades/trade_' + str(trade_number) + '.csv'
     tick_data_file_path = file_mapping[selected_timeframe]
     trade_data = load_trade_data(trade_data_file_path)
     tf_ohlc_data = load_tick_data(tick_data_file_path)
 
     # Organizing the dataframe
     desired_columns = [
-        'Trade Number', 'Open DateTime', 'Price', 'Type', 
-        'S / L', 'T / P', 'Close DateTime', 'Price.1'
+        'Open DateTime', 'Opening Price', 'Type', 
+        'S / L', 'T / P', 'Close DateTime', 'Closing Price'
     ]
     trade_data = trade_data[desired_columns]
-    trade_data.rename(columns={
-        'Price': 'Open Price',
-        'Price.1': 'Close Price'
-    }, inplace=True)
-
-    selected_trades = select_trades_by_number(trade_data)
 
     # Inputting display settings
     input_settings = {}
     input_settings["Candles Before"] = st.number_input('Candles Before Trade', min_value=0, value=5)
     input_settings["Candles After"] = st.number_input('Candles After Trade', min_value=0, value=5)
 
-    # Initialize an empty dictionary to store the user's choices
-    yesterday_ohlc_questions = ["Display yesterday's high?", 
-                                "Display yesterday's low?",
-                                "Display yesterday's open?",
-                                "Display yesterday's close?"]
-    for question in yesterday_ohlc_questions:
-        input_settings[question] = st.toggle(question)
-    if any(input_settings[key] for key in yesterday_ohlc_questions):
-        input_settings["Daily OHLC Data"] = load_tick_data(file_mapping['1D'])
-
-    input_settings["Display RSI?"] = st.toggle("Display RSI?", value=True)
-    if input_settings["Display RSI?"]:
-        input_settings["RSI Length"] = st.number_input("RSI Length", value=21, min_value=1)
-
-    rows, cols = selected_trades.shape
+    rows, cols = trade_data.shape
     if rows != 0:
         chart = create_candlestick_chart(
             tf_ohlc_data,
-            selected_trades,
+            trade_data,
             selected_timeframe,
             input_settings
         )
